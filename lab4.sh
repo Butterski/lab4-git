86110bcf53626b0910531c0b87d9da5bc8a4f6b6
milosz.kucharski <miloszkucharski@gmail.com>
2023-04-12 19:18:51 +0200

    added '--logs'

--- lab4.sh
+++ lab4.sh
@@ -2,3 +2,14 @@
 
 if [[ $1 == "--date" ]]; then
   date +"%Y-%m-%d"
+
+elif [[ $1 == "--logs" ]]; then
+  if [[ $2 =~ ^[0-100]+$ ]]; then
+    for ((i=1; i<=$2; i++)); do
+      filename="log$i.txt"
+      echo -e "Nazwa pliku: $filename\nNazwa skryptu: $0\nData: $(date)" > $filename
+    done
+  else
+    echo "Nieprawidłowy argument: wymagana liczba plików"
+fi
+fi
