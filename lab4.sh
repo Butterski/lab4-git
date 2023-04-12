f03610dc81a0fff8e44e541bb5b3bed8a22d36fb
milosz.kucharski <miloszkucharski@gmail.com>
2023-04-12 19:30:54 +0200

    format

--- lab4.sh
+++ lab4.sh
@@ -4,7 +4,7 @@ if [[ $1 == "--date" ]]; then
   date +"%Y-%m-%d"
 
 elif [[ $1 == "--logs" ]]; then
-  if [[ $2 =~ ^[0-9]+$ ]]; then
+  if [[ $2 =~ ^[0-100]+$ ]]; then
     for ((i=1; i<=$2; i++)); do
       filename="log$i.txt"
       echo -e "Nazwa pliku: $filename\nNazwa skryptu: $0\nData: $(date)" > $filename
